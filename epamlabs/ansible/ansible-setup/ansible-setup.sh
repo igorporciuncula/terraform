9dedce014714d29e0d1578b25d3e367c9b1001de
Thiago Magalhaes <tpmagalhaes@gmail.com>
2020-03-19 17:30:14 +0000

    commit_1_19032020

--- epamlabs/ansible/ansible-setup/ansible-setup.sh
+++ epamlabs/ansible/ansible-setup/ansible-setup.sh
@@ -1,2 +1,10 @@
+#Shell que faz a chamada da instalacao do Ansible (Fedora, CentOS, Rhel)
 
-sudo yum install ansible 
+#!/bin/bash
+echo "Atualizando os pacotes..."
+yum update -y
+echo "Instalando Ansible..."
+yum install ansible -y
+exit 0
+
+
